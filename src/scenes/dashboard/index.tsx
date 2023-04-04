984bc5dcfcc861a1f0b7e0717fabda0a385ba7b3
devLucasmarts <martssilvapro@gmail.com>
2023-04-04 16:01:02 -0300

    config small grid

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,6 +1,4 @@
-import { useTheme } from "@emotion/react";
-import { Box } from "@mui/material";
-
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 
 type Props = {}
 
@@ -8,7 +6,7 @@ const Dashboard = (props: Props) => {
 
   const { palette } = useTheme();
 
-  const gridTemplate = `
+  const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -20,16 +18,59 @@ const Dashboard = (props: Props) => {
     "g h j"
     "g h j"
   `
+  const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+    "i"
+    "i"
+    "j"
+    "j"
+  ` 
+
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1200px)");
 
   return (
     <Box color={palette.grey[300]} width="100%" height="100%" display="grid" gap="1.5rem"
       sx={{
         gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
         gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        gridTemplateAreas: gridTemplate,
+        gridTemplateAreas: gridTemplateLargeScreens,
       }}
     >
-      teste
+      <Box bgcolor="#FFF"  gridArea="a"></Box>
+      <Box bgcolor="#FFF" gridArea="b"></Box>
+      <Box bgcolor="#FFF" gridArea="c"></Box>
+      <Box bgcolor="#FFF" gridArea="d"></Box>
+      <Box bgcolor="#FFF" gridArea="e"></Box>
+      <Box bgcolor="#FFF" gridArea="f"></Box>
+      <Box bgcolor="#FFF" gridArea="g"></Box>
+      <Box bgcolor="#FFF" gridArea="h"></Box>
+      <Box bgcolor="#FFF" gridArea="i"></Box>
+      <Box bgcolor="#FFF" gridArea="j"></Box>
     </Box>
   );
 };
