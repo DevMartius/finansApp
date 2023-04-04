1b6e47558549422d4db5be97b30f5b4e84740005
devLucasmarts <martssilvapro@gmail.com>
2023-04-04 15:38:27 -0300

    config grid template

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,11 +1,37 @@
+import { useTheme } from "@emotion/react";
+import { Box } from "@mui/material";
 
 
 type Props = {}
 
-const index = (props: Props) => {
+const Dashboard = (props: Props) => {
+
+  const { palette } = useTheme();
+
+  const gridTemplate = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d h i"
+    "g h i"
+    "g h j"
+    "g h j"
+  `
+
   return (
-    <div>index</div>
+    <Box color={palette.grey[300]} width="100%" height="100%" display="grid" gap="1.5rem"
+      sx={{
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplate,
+      }}
+    >
+      teste
+    </Box>
   );
 };
 
-export default index;
+export default Dashboard;
